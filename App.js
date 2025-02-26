61e396d20e77bfa970a1be05ff7f220bc056bb41
juan3-1416 <jpmonsa1907@gmail.com>
2025-02-26 01:03:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Aca toca programar</h1>
+            <h3>Aca toca implementar un react navigation</h3>
+            <img src="http://127.0.0.1:5500/landing/wp-content/uploads/2024/08/logo-app-small.png"/>
+        </div>
+    );
+}
+export default App;
